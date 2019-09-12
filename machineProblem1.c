0b17de85e2e1c0cf0190dda84cffa4514f25aaee
Genevieve Saab <gensaab@gmail.com>
2019-09-12 12:13:38 -0500

    Update: add getSize

--- machineProblem1.c
+++ machineProblem1.c
@@ -1,3 +1,44 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-//comment
+//Function prototypes
+int getSize(FILE *) //Scans input file to find largest vertex to obtain size of adjacency list
+
+int main(int argc, char *argv[])
+{
+    FILE *input;
+
+    if(argc > 3 || argc < 3)
+    {
+        puts("Incorrect number of arguments.");
+        return 0;
+    }
+
+    if((input = fopen(argv[1], "r")) == NULL)
+    {
+        puts("Unable to open the input file.");
+        return 0;
+    }
+    return 0;
+}
+
+int getSize(FILE *input)
+{
+    int numVertex = 0;
+
+    char c = getc(input);
+    while(c != EOF)
+    {
+        if (c > numVertex)
+        {
+            numVertex = c;
+        }
+    }
+
+    size = numVertex + 1;
+
+    rewind(input)
+    fclose(input);
+    return size;
+}
+
