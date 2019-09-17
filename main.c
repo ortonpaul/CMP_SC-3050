7f5bfbe9ee58fd8c3f195450d3a27a0a2fc9b149
Paul Orton <ortonpaul18@gmail.com>
2019-09-16 23:02:16 -0500

    Added G^2 Logic

--- main.c
+++ main.c
@@ -1,45 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Function prototypes
-//int getSize(char *); //Scans input file to find largest vertex to obtain size of adjacency list
 struct node
 {
     int vertex; //vertex number
     struct node* next; //pointer to next node in linked list
-};
+} Node;
 struct node* createNode(int); //creates node in linked list
 
 struct Graph
 {
     int numVertices;
-    int color; //white, gray or black
     struct node** adjLists;
 };
 
-
+// Function Prototypes
 struct Graph* createGraph(int vertices); //passes size of graph as parameter and creates null graph
 void addEdge(struct Graph* graph, int src, int dest); //adds edge for directed graph
 void printGraph(struct Graph* graph);
+int getVertices(void); //Gets number of vertices of graph to be made
+int getEdges(void); //Gets number of edges of graph to be made
 
 
 int main(int argc, char *argv[])
 {
-    int size = 6;
-    struct Graph* graph = createGraph(size);
+    int vertices = getVertices() + 1;
+    int edges = getEdges();
+    struct Graph* graph = createGraph(vertices);
+    struct Graph* graph2 = createGraph(vertices);
 
     FILE *file;
     file = fopen("input.txt", "r");
 
-    int source[size];
-    int destination[size];
-    for(int i = 0; i < size; i++)
+    int source[edges];
+    int destination[edges];
+    for (int i = 0; i < edges; i++)
     {
         fscanf(file, "%d %d", &source[i], &destination[i]);
         addEdge(graph, source[i], destination[i]);
     }
 
-    printGraph(graph);
+    struct node *ptr = NULL;
+    struct node *ptr2 = NULL;
+    int u, v, w;
+
+    for (u = 1; u < vertices; u++) {
+        if(graph -> adjLists[u] != NULL) {
+            ptr = graph -> adjLists[u];
+            while (ptr) {
+                v = ptr -> vertex;
+
+                ptr2 = graph -> adjLists[v];
+
+                while (ptr2) {
+                    w = ptr2 -> vertex;
+                        
+
+                        if (u != w) {
+                            addEdge(graph2, u, w);
+                        }
+
+
+                    ptr2 = ptr2 -> next;
+                }
+                ptr = ptr -> next;
+            }
+        } else {
+            continue;
+        }
+    }
+
+    printGraph(graph2);
+
+    fclose(file);
 
     return 0;
 }
@@ -77,10 +110,11 @@ void addEdge(struct Graph* graph, int src, int dest)
 void printGraph(struct Graph* graph)
 {
     int v;
-    for (v = 0; v < graph->numVertices; v++)
+    printf("\n\t[OUTPUT]\n\n");
+    for (v = 1; v < graph->numVertices; v++)
     {
         struct node* temp = graph->adjLists[v];
-        printf("\n Adjacency list of vertex %d\n ", v);
+        printf("%d: ", v);
         while (temp)
         {
             printf("%d ", temp->vertex);
@@ -88,4 +122,41 @@ void printGraph(struct Graph* graph)
         }
         printf("\n");
     }
+    printf("\n");
+}
+
+int getVertices(void) {
+    FILE *file;
+    file = fopen("input.txt", "r");
+
+    int size = 0;
+    int tempSize = 0;
+
+    while(!feof(file)) {
+        fscanf(file, "%d", &tempSize);
+        if (tempSize > size) {
+            size = tempSize;
+        }
+    }
+
+    fclose(file);
+
+    return size;
+}
+
+int getEdges(void) {
+    FILE *file;
+    file = fopen("input.txt", "r");
+
+    int edges = 0;
+
+    while(!feof(file)) {
+        if(getc(file) == '\n') {
+            edges++;
+        }
+    }
+
+    fclose(file);
+
+    return edges;
 }
